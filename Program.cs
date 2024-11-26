ee3d50ada42db58a4d0b365b6dce5325bde5eeca
_Coder_Naren <narendrajalamkar8399@gmail.com>
2024-11-26 10:08:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_StateCodePhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Displaying Messages
+            Console.WriteLine("Enter Your PhoneNumber :");
+            //Getting number from user
+            long PhoneNumber = Convert.ToInt64(Console.ReadLine());
+
+            //Concatination of PhoneNumber
+            Console.Write("Phone Number is" + PhoneNumber);
+        }
+    }
+}
